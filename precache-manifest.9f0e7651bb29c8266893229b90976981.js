67415ddf83a031a6ca2a902d35e5dccc82e58f90
Oleksandr <sashaka2013@gmail.com>
2019-11-10 22:59:50 +0200

    Updates

---
+++ precache-manifest.9f0e7651bb29c8266893229b90976981.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e634bee9ff5ee0d62e98ef069ad76398",
+    "url": "/equipment-rent/index.html"
+  },
+  {
+    "revision": "413ecaa8f72e2b1a5551",
+    "url": "/equipment-rent/static/css/main.b2ae16c9.chunk.css"
+  },
+  {
+    "revision": "39a6dd86bbe567042f6f",
+    "url": "/equipment-rent/static/js/2.30e9fb07.chunk.js"
+  },
+  {
+    "revision": "413ecaa8f72e2b1a5551",
+    "url": "/equipment-rent/static/js/main.9f34531d.chunk.js"
+  },
+  {
+    "revision": "ea5d809a84183f46729b",
+    "url": "/equipment-rent/static/js/runtime-main.d81edaea.js"
+  },
+  {
+    "revision": "c6cdd24327eae078216190d79e761135",
+    "url": "/equipment-rent/static/media/camera.c6cdd243.jpg"
+  },
+  {
+    "revision": "325974414104b49185451ac7117b77fc",
+    "url": "/equipment-rent/static/media/lights.32597441.jpg"
+  },
+  {
+    "revision": "9e73131b9773e166ef8d0153a7a7ca27",
+    "url": "/equipment-rent/static/media/microphone.9e73131b.jpg"
+  },
+  {
+    "revision": "150889652c97dc0bbbb5d8f55619670b",
+    "url": "/equipment-rent/static/media/sf-pro-display_light.15088965.woff2"
+  },
+  {
+    "revision": "12c19fb6e4b532d28dde0248507f95c3",
+    "url": "/equipment-rent/static/media/sf-pro-display_regular.12c19fb6.woff2"
+  },
+  {
+    "revision": "a1657f438dbcb536bf7bf28ab76e9ecf",
+    "url": "/equipment-rent/static/media/sf-pro-display_semibold.a1657f43.woff2"
+  }
+]);
