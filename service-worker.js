5e0a1dc0e29414ed6b6d2f69b1f090ee0afaaff1
Oleksandr <sashaka2013@gmail.com>
2020-01-22 00:42:34 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/equipment-rent/precache-manifest.e63851d495d05a782515294754c0aa29.js"
+  "/equipment-rent/precache-manifest.db682f3c528b1a7b9554921cb0fba186.js"
 );
 
 self.addEventListener('message', (event) => {
